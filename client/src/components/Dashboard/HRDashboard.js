383aaf3306e1b3a006c167089d4a5d205aecc751
jlassimohamed-99 <jlayssenzlala@gmail.com>
2024-06-03 18:16:28 +0200

    Revert "7atta_11"
    This reverts commit 7e5218b1200d262d380b9884bfbb5ef055ec77a7.

--- client/src/components/Dashboard/HRDashboard.js
+++ client/src/components/Dashboard/HRDashboard.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UserManagement from '../UserManagement';
-import LeftNav from '../LeftNav/LeftNav';
-import MainSection from '../MainSection/MainSection';
-import TopNav from '../TopNav/TopNav';
-import '../../App.css'; // Assurez-vous que le chemin vers App.css est correct
 
 const HRDashboard = () => {
   const [employees, setEmployees] = useState([]);
-  const [employeeId, setEmployeeId] = useState('');
 
   useEffect(() => {
     const fetchEmployees = async () => {
@@ -27,20 +22,15 @@ const HRDashboard = () => {
   }, []);
 
   return (
-    <div className="App">
-      <TopNav />
-      <LeftNav employeeId={employeeId} />
-      <MainSection setEmployeeId={setEmployeeId} />
-      <div>
-        <h2>HR Dashboard</h2>
-        <UserManagement />
-        <h3>Employee List</h3>
-        <ul>
-          {employees.map(employee => (
-            <li key={employee._id}>{employee.name} - {employee.role}</li>
-          ))}
-        </ul>
-      </div>
+    <div>
+      <h2>HR Dashboard</h2>
+      <UserManagement />
+      <h3>Employee List</h3>
+      <ul>
+        {employees.map(employee => (
+          <li key={employee._id}>{employee.name} - {employee.role}</li>
+        ))}
+      </ul>
     </div>
   );
 };
